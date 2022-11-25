9003caabcd6196abe7202d28f89491fa2f4d800e
Roman Lamsal <roman@lamsal.de>
2022-11-25 13:16:34 +0100

    improvement: add version logging and command

--- packages/cli/cli.ts
+++ packages/cli/cli.ts
@@ -3,7 +3,24 @@
 import { program } from "commander"
 import serverCommand from "@gueterbahnhof/server"
 import clientCommand from "@gueterbahnhof/client"
+import { existsSync, readFileSync } from "fs"
+import { fileURLToPath } from "url"
 
-program.addCommand(serverCommand).addCommand(clientCommand)
+function getPackageJson() {
+    for (const relativePath of ["../package.json", "./package.json"]) {
+        const absoluteUrl = fileURLToPath(
+            new URL(relativePath, import.meta.url.startsWith("file://") ? import.meta.url : `file://${import.meta.url}`)
+        )
+        if (existsSync(absoluteUrl)) {
+            return absoluteUrl
+        }
+    }
+
+    return
+}
+
+const version = JSON.parse(readFileSync(getPackageJson()).toString()).version
+
+program.version(version).addCommand(serverCommand(version)).addCommand(clientCommand)
 
 program.parse(process.argv)
